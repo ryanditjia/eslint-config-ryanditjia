73e316ccca3f50a1a8642487010ba03c62d92d65
Ryandi Tjia <ryandi.tjia@gmail.com>
2018-11-14 11:20:48 +0700

    Experiment with warn instead of error for no-unused-vars and prettier

--- index.js
+++ index.js
@@ -5,4 +5,13 @@ module.exports = {
     browser: true,
   },
   extends: ['airbnb', 'plugin:prettier/recommended'],
+  rules: {
+    // experiment: overriding Airbnb’s error as warn
+    'no-unused-vars': [
+      'warn',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: true },
+    ],
+    // experiment: overriding Prettier’s error as warn
+    'prettier/prettier': 'warn',
+  },
 }
